dab2bcff2780f24977fb480d33cc11de19006e5e
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2017-10-20 10:18:29 +0100

    Added description of flags from documentation to mod_cpl_flags

--- source/mod_cpl_flags.f90
+++ source/mod_cpl_flags.f90
@@ -7,14 +7,23 @@ module mod_cpl_flags
     public icland, icsea, icice, isstan
 
     ! Flag for land-coupling
+    ! 0=no, 1=land-model
     integer :: icland = 1
 
     ! Flag for sea (SST) coupling
+    ! 0 = precribed SST, no coupling
+    ! 1 = precribed SST, ocean model forced by atm.
+    ! 2 = full (uncorrected) SST from coupled ocean model
+    ! 3 = SST anomaly from coupled ocean model + obs. SST clim.
+    ! 4 = as 3 with prescribed SST anomaly in ElNino region
     integer :: icsea  = 0
 
     ! Flag for sea-ice coupling
+    ! 0=no, 1=ice-model
     integer :: icice  = 1
 
     ! Flag for observed SST anomaly
+    ! 0 = no (clim. SST), 1 = observed anomaly
+    ! (active if ICSEA = 0, 1; set to 1 if ICSEA = 4)
     integer :: isstan = 1
 end module
