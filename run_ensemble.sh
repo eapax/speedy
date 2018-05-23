0846b4e140ca9a05a5f60ef04435ec723161455d
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2018-05-23 12:36:51 +0100

    Updated run_ensemble to be consistent with default run script

--- run_ensemble.sh
+++ run_ensemble.sh
@@ -1,126 +1,78 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # $1 = resolution (eg t21, t30)
 # $2 = experiment no. (eg 111)
 # $3 = experiment no. for restart file ( 0 = no restart )
-# $4 = make only, and don't run? ("make" for yes, "run" for no)
 
 
-if [ $# -ne 4 ] ; then
-    echo 'Usage: '$0' resol. exp_no. restart_no make_only_or_run' 1>&2
+if [ $# -ne 3 ] ; then
+    echo 'Usage: '${0}' resol. exp_no. restart_no' 1>&2
     exit 1
 fi
 
-# Start date
-year='1982'
-month='01'
-day='01'
-hour='00'
-
+# Define precisions to test
+pmin=5
+pmax=23
 # Define directory names
 UT=`pwd`
-SRC=${UT}/source
-TMP=${UT}/temp
-mkdir -p ${UT}/output/exp_$2
-OUT=${UT}/output/exp_$2
-CD=${UT}/output/exp_$3
+TMP=/home/saffin/temp/
+mkdir -p ${UT}/output/exp_${2}
+OUT=${UT}/output/exp_${2}
+INP=${UT}/output/exp_${3}
+CTL=${UT}/output/exp_000
+
+# Setup files
+executable=${UT}/source/imp.exe
+namelist=${UT}/setup/speedy.nml
 
 # Copy files from basic version directory
-echo "copying from $SRC/source to $TMP"
 mkdir -p ${TMP}
 cd ${TMP}
 rm *
-cp ${OUT}/yyyymmddhh_p.ctl ${TMP}/
-cp ${SRC}/*.f90      ${TMP}/
-cp ${SRC}/*.h      ${TMP}/
-cp ${SRC}/*.s      ${TMP}/
-cp ${SRC}/makefile ${TMP}/
+cp ${executable} ${TMP}/imp.exe
+cp ${namelist}   ${TMP}/speedy.nml
+cp ${CTL}/*.ctl ${TMP}
+cp ${UT}/setup/output_requests.nml ${TMP}
+cp ${UT}/setup/precisions.nml ${TMP}
+
+# Link restart file if needed
+if [ ${3} != 0 ] ; then
+  ln -s ${INP}/*.rst ${TMP}
+fi
 
 # Link input files
-echo 'link input files to fortran units'
-sh inpfiles.s $1
+SB=${UT}/data/bc/${1}/clim
+SC=${UT}/data/bc/${1}/anom
 
-ls -l fort.*
-
-echo ' compiling at_gcm - calling make'
-
-make clean
-make imp.exe || { echo "Compilation failed"; exit 1; }
-
-if [ $4 == make ] ; then
-    exit 0
-fi
-
-# Write date input file
-if [ $3 = 0 ]; then
-    echo 0 > fort.2
-else
-    echo "link restart file ${year}${month}${day}${hour}"
-    ln -s ${CD}/${year}${month}${day}${hour}.rst
-    echo 1 > fort.2
-fi
-echo ${year} >> fort.2
-echo ${month} >> fort.2
-echo ${day} >> fort.2
-echo ${hour} >> fort.2
+ln -sf ${SB}/sfc.grd   fort.20
+ln -sf ${SB}/sst.grd   fort.21
+ln -sf ${SB}/icec.grd  fort.22
+ln -sf ${SB}/stl.grd   fort.23
+ln -sf ${SB}/snowd.grd fort.24
+ln -sf ${SB}/swet.grd  fort.26
+ln -sf ${SC}/ssta.grd  fort.30
 
 
 # Loop over precisions being tested
-for i in {5..52}
+for i in $(seq ${pmin} ${pmax})
 do
     echo ${i}
     # Write precision to input file
     # reduced_precision
-    echo ${i} > precision.txt
-    # initial values
-    echo ${i} >> precision.txt
-    # spectral transform
-    echo ${i} >> precision.txt
-    # grid physics
-    echo ${i} >> precision.txt
-    # convection
-    echo ${i} >> precision.txt
-    # condensation
-    echo ${i} >> precision.txt
-    # short-wave radiation
-    echo ${i} >> precision.txt
-    # long-wave radiation
-    echo ${i} >> precision.txt
-    # surface fluxes
-    echo ${i} >> precision.txt
-    # vertical diffusion
-    echo ${i} >> precision.txt
-    # SPPT
-    echo ${i} >> precision.txt
-    # grid dynamics
-    echo ${i} >> precision.txt
-    # spectral dynamics
-    echo ${i} >> precision.txt
-    # diffusion
-    echo ${i} >> precision.txt
-    # time stepping
-    echo ${i} >> precision.txt
-    # Prognostics
-    echo ${i} >> precision.txt
-    # Tendencies
-    echo ${i} >> precision.txt
-    # Initialisation
-    echo ${i} >> precision.txt
-    # Parameters
-    echo ${i} >> precision.txt
-
+    sed -i "s/rp_convection=.*/rp_convection=${i},/" precisions.nml
+    cat precisions.nml
 
     # Run the model
     time ./imp.exe | tee out.lis
 
     # Convert .grd output to .nc
-    grd2nc_p.sh yyyymmddhh_p.ctl
+    grd2nc_p.sh prognostics_pressure.ctl
+    grd2nc.sh tendencies.ctl
 
     # Move to a unique file labelled by the precision
-    mv yyyymmddhh_p.nc ${OUT}/yyyymmddhh_p${i}.nc
+    mv prognostics_pressure.nc ${OUT}/prognostics_pressure_${i}.nc
+    mv tendencies.nc ${OUT}/tendencies_${i}.nc
 
     # Remove model output
-    mv out.lis ${OUT}/atgcm$2.lis
     rm *.grd
-    rm precision.txt
 done
