b43104b736381ac1575d2acc2cf4f4549081e823
Leo Saffin <leo.saffin@physics.ox.ac.uk>
2019-07-05 11:55:55 +0100

    Delete links and directories in run script as well as files

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 
 set -e
 
-if [ $# -ne 3 ] ; then
+if (($# != 3)); then
     echo 'Usage: '${0}' resolution, experiment no., restart no.' 1>&2
     exit 1
 fi
@@ -24,14 +24,16 @@ output=${UT}/setup/default_outputs.nml
 
 # Copy files from basic version directory
 mkdir -p ${TMP}
-find ${TMP}/* -type f -delete
+find ${TMP} -type f -delete
+find ${TMP} -type l -delete
+find ${TMP} -type d -delete
 cp ${executable} ${TMP}/imp.exe
 cp ${namelist}   ${TMP}/speedy.nml
 cp ${output}     ${TMP}/output_requests.nml
 
 # Link restart file if needed
-if [ ${3} != 0 ] ; then
-  cp -sf ${INP}/*.rst ${TMP}
+if ((${3} != 0)); then
+  cp ${INP}/*.rst ${TMP}
 fi
 
 # Link input files
@@ -39,9 +41,9 @@ BC=${UT}/data/bc/${1}
 SH=${UT}/hflux
 
 cd ${TMP}
-ln -sf ${BC}/climatology.nc climatology.nc
-ln -sf ${BC}/anomalies.nc   anomalies.nc
-ln -sf ${SH}/hflux_speedy_ver41_1979_2008_clim.grd fort.31
+ln -s ${BC}/climatology.nc climatology.nc
+ln -s ${BC}/anomalies.nc   anomalies.nc
+ln -s ${SH}/hflux_speedy_ver41_1979_2008_clim.grd fort.31
 
 # Link netCDF library
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/share/netcdf/lib
