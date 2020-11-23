b147de1f6c752bb29287c3fbcf4b497fa8928fb4
Adam Paxton <paxton@physics.ox.ac.uk>
2020-11-23 16:58:00 +0000

    batch run for 06sbits (control SST forcing)

--- qrun.sh
+++ qrun.sh
@@ -2,8 +2,13 @@
 
 #sbatch --job-name=EN52s11eMEMBER0 ./newqueue.sh EN52s11eMEMBER0 060 1 52
 
+#for i in {0..4}
+#do
+#	sbatch --job-name=SR_EN10s11eMEM$i ./newqueue.sh SR_EN10s11eMEM$i 06$i 1 10
+#	sbatch --job-name=SR_CONT10s11eMEM$i ./newqueue.sh SR_CONT10s11eMEM$i 01$i 0 10
+#done
+
 for i in {0..4}
-do
-	sbatch --job-name=SR_EN10s11eMEM$i ./newqueue.sh SR_EN10s11eMEM$i 06$i 1 10
-	sbatch --job-name=SR_CONT10s11eMEM$i ./newqueue.sh SR_CONT10s11eMEM$i 01$i 0 10
+do 
+	sbatch --job-name=SR_CONT06s11eMEM$i ./newqueue.sh SR_CONT06s11eMEM$i 01$i 0 06
 done
