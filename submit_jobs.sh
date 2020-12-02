cbf414a1ffb71b18a92533d04bd66d2ad2bbb1c2
Adam Paxton <paxton@physics.ox.ac.uk>
2020-12-02 09:32:22 +0000

    new workflow

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-#sbatch --job-name=EN52s11eMEMBER0 ./newqueue.sh EN52s11eMEMBER0 060 1 52
-
 #for i in {0..4}
 #do
-#	sbatch --job-name=SR_EN10s11eMEM$i ./newqueue.sh SR_EN10s11eMEM$i 06$i 1 10
-#	sbatch --job-name=SR_CONT10s11eMEM$i ./newqueue.sh SR_CONT10s11eMEM$i 01$i 0 10
+#	sbatch --job-name=SR_EN10s11eMEM$i Q.sh ./run10year.sh SR_EN10s11eMEM$i 06$i 1 10
+#	sbatch --job-name=SR_CONT10s11eMEM$i Q.sh ./run10year.sh SR_CONT10s11eMEM$i 01$i 0 10
 #done
 
 for i in {0..4}
 do
-	sbatch --job-name=SR_RPEFALSE_CONT10s11eMEM$i ./newqueue.sh SR_RPEFALSE_CONT10s11eMEM$i 01$i 0 10RPEFALSE
+	sbatch --job-name=SR_RPEFALSE_CONT10s11eMEM$i Q.sh ./run10year.sh SR_RPEFALSE_CONT10s11eMEM$i 01$i 0 10RPEFALSE
 done
 
-#for i in {0..4}
-#do 
-#	sbatch --job-name=SR_CONT06s11eMEM$i ./newqueue.sh SR_CONT06s11eMEM$i 01$i 0 06
-#done
+for i in {0..4}
+do
+	sbatch --job-name=SR_CONT08s11eMEM$i Q.sh ./run10year.sh SR_CONT08s11eMEM$i 01$i 0 08
+	sbatch --job-name=SR_CONT09s11eMEM$i Q.sh ./run10year.sh SR_CONT09s11eMEM$i 01$i 0 09
+done
 
-#for i in {0..4}
-#do
-#	sbatch --job-name=SR_CONT07s11eMEM$i ./newqueue.sh SR_CONT07s11eMEM$i 01$i 0 07
-#done
+for i in {0..4}
+do 
+	sbatch --job-name=SR_CONT23s11eMEM$i Q.sh ./run10year.sh SR_CONT23s11eMEM$i 01$i 0 23
+done
 
-#for i in {0..4}
-#do
-#	sbatch --job-name=SR_CONT08s11eMEM$i ./newqueue.sh SR_CONT08s11eMEM$i 01$i 0 08
-#	sbatch --job-name=SR_CONT09s11eMEM$i ./newqueue.sh SR_CONT09s11eMEM$i 01$i 0 09
-#done
-
-#for i in {0..4}
-#do 
-#	sbatch --job-name=SR_EN23s11eMEM$i ./newqueue.sh SR_EN23s11eMEM$i 06$i 1 23
-#	sbatch --job-name=SR_CONT23s11eMEM$i ./newqueue.sh SR_CONT23s11eMEM$i 01$i 0 23
-#done
-#
