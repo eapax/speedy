34d9633f81080b4406c9f0f062f09f6ea7e53505
Tom Kimpson <tomkimpson@gmail.com>
2021-10-09 09:14:06 +0100

    set old settings for 1 year run

--- batch.sh
+++ batch.sh
@@ -1,28 +1,28 @@
 #L1 - EL NINO BASIC
-nohup time ./run.sh speedyone_L1_50_RN_new_1year 010 1 SRoff50 1year > output/speedyone-L1_50_RN_new.out &
-nohup time ./run.sh speedyone_L1_23_RN_new_1year 010 1 SRoff23 1year > output/speedyone-L1_23_RN_new.out &
-nohup time ./run.sh speedyone_L1_10_RN_new_1year 010 1 SRoff10 1year > output/speedyone-L1_10_RN_new.out &
-nohup time ./run.sh speedyone_L1_10_SR_new_1year 010 1 10      1year > output/speedyone-L1_10_SR_new.out &
+nohup time ./run.sh speedyone_L1_50_RN_old_1year 010 1 SRoff50 1year > output/speedyone-L1_50_RN_old.out &
+nohup time ./run.sh speedyone_L1_23_RN_old_1year 010 1 SRoff23 1year > output/speedyone-L1_23_RN_old.out &
+nohup time ./run.sh speedyone_L1_10_RN_old_1year 010 1 SRoff10 1year > output/speedyone-L1_10_RN_old.out &
+nohup time ./run.sh speedyone_L1_10_SR_old_1year 010 1 10      1year > output/speedyone-L1_10_SR_old.out &
 
 
-nohup time ./run.sh speedyone_L2_50_RN_new_1year 010 2 SRoff50 1year > output/speedyone-L2_50_RN_new.out &
-nohup time ./run.sh speedyone_L2_23_RN_new_1year 010 2 SRoff23 1year > output/speedyone-L2_23_RN_new.out &
-nohup time ./run.sh speedyone_L2_10_RN_new_1year 010 2 SRoff10 1year > output/speedyone-L2_10_RN_new.out &
-nohup time ./run.sh speedyone_L2_10_SR_new_1year 010 2 10      1year > output/speedyone-L2_10_SR_new.out &
+nohup time ./run.sh speedyone_L2_50_RN_old_1year 010 2 SRoff50 1year > output/speedyone-L2_50_RN_old.out &
+nohup time ./run.sh speedyone_L2_23_RN_old_1year 010 2 SRoff23 1year > output/speedyone-L2_23_RN_old.out &
+nohup time ./run.sh speedyone_L2_10_RN_old_1year 010 2 SRoff10 1year > output/speedyone-L2_10_RN_old.out &
+nohup time ./run.sh speedyone_L2_10_SR_old_1year 010 2 10      1year > output/speedyone-L2_10_SR_old.out &
 
 
 #L3 - L1 + ablco2 level 6---> 21
-#nohup time ./run.sh speedyone_L3_50_RN_new_1year 010 1 SRoff50 1year_ablco2 > output/speedyone-L3_50_RN_new.out &
-#nohup time ./run.sh speedyone_L3_23_RN_new_1year 010 1 SRoff23 1year_ablco2 > output/speedyone-L3_23_RN_new.out &
-#nohup time ./run.sh speedyone_L3_10_RN_new_1year 010 1 SRoff10 1year_ablco2 > output/speedyone-L3_10_RN_new.out &
-#nohup time ./run.sh speedyone_L3_10_SR_new_1year 010 1 10      1year_ablco2 > output/speedyone-L3_10_SR.out &
+#nohup time ./run.sh speedyone_L3_50_RN_old_1year 010 1 SRoff50 1year_ablco2 > output/speedyone-L3_50_RN_old.out &
+#nohup time ./run.sh speedyone_L3_23_RN_old_1year 010 1 SRoff23 1year_ablco2 > output/speedyone-L3_23_RN_old.out &
+#nohup time ./run.sh speedyone_L3_10_RN_old_1year 010 1 SRoff10 1year_ablco2 > output/speedyone-L3_10_RN_old.out &
+#nohup time ./run.sh speedyone_L3_10_SR_old_1year 010 1 10      1year_ablco2 > output/speedyone-L3_10_SR.out &
 
 
 #L4- Both i.e. 4xCO2 run
-#nohup time ./run.sh speedyone_L4_50_RN_new_1year 010 2 SRoff50 1year_ablco2 > output/speedyone-L4_50_RN_new.out &
-#nohup time ./run.sh speedyone_L4_23_RN_new_1year 010 2 SRoff23 1year_ablco2 > output/speedyone-L4_23_RN_new.out &
-#nohup time ./run.sh speedyone_L4_10_RN_new_1year 010 2 SRoff10 1year_ablco2 > output/speedyone-L4_10_RN_new.out &
-#nohup time ./run.sh speedyone_L4_10_SR_new_1year 010 2 10      1year_ablco2 > output/speedyone-L4_10_SR.out &
+#nohup time ./run.sh speedyone_L4_50_RN_old_1year 010 2 SRoff50 1year_ablco2 > output/speedyone-L4_50_RN_old.out &
+#nohup time ./run.sh speedyone_L4_23_RN_old_1year 010 2 SRoff23 1year_ablco2 > output/speedyone-L4_23_RN_old.out &
+#nohup time ./run.sh speedyone_L4_10_RN_old_1year 010 2 SRoff10 1year_ablco2 > output/speedyone-L4_10_RN_old.out &
+#nohup time ./run.sh speedyone_L4_10_SR_old_1year 010 2 10      1year_ablco2 > output/speedyone-L4_10_SR.out &
 
 
 
