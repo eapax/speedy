8923b30bc00833c873a1509e9bec3d7ab774bd68
Tom Kimpson <tomkimpson@gmail.com>
2021-10-15 14:18:14 +0100

    ts plus diffusion at 50

--- batch.sh
+++ batch.sh
@@ -1,28 +1,28 @@
 #L1 - EL NINO BASIC
-nohup time ./run.sh speedyone_L1_50_RN_24_hour_mod_ts_and_convection 010 1 SRoff50 24hour > output/speedyone-L1_50_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L1_23_RN_24_hour_mod_ts_and_convection 010 1 SRoff23 24hour > output/speedyone-L1_23_RN_24_hour_mod_ts_and_convection.out &
-nohup time ./run.sh speedyone_L1_10_RN_24_hour_mod_ts_and_convection 010 1 SRoff10 24hour > output/speedyone-L1_10_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L1_10_SR_24_hour_mod_ts_and_convection 010 1 10      24hour > output/speedyone-L1_10_SR_24_hour_mod_ts_and_convection.out &
+nohup time ./run.sh speedyone_L1_50_RN_24_hour_mod_ts_and_diffusion 010 1 SRoff50 24hour > output/speedyone-L1_50_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L1_23_RN_24_hour_mod_ts_and_diffusion 010 1 SRoff23 24hour > output/speedyone-L1_23_RN_24_hour_mod_ts_and_diffusion.out &
+nohup time ./run.sh speedyone_L1_10_RN_24_hour_mod_ts_and_diffusion 010 1 SRoff10 24hour > output/speedyone-L1_10_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L1_10_SR_24_hour_mod_ts_and_diffusion 010 1 10      24hour > output/speedyone-L1_10_SR_24_hour_mod_ts_and_diffusion.out &
 
 
-nohup time ./run.sh speedyone_L2_50_RN_24_hour_mod_ts_and_convection 010 2 SRoff50 24hour > output/speedyone-L2_50_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L2_23_RN_24_hour_mod_ts_and_convection 010 2 SRoff23 24hour > output/speedyone-L2_23_RN_24_hour_mod_ts_and_convection.out &
-nohup time ./run.sh speedyone_L2_10_RN_24_hour_mod_ts_and_convection 010 2 SRoff10 24hour > output/speedyone-L2_10_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L2_10_SR_24_hour_mod_ts_and_convection 010 2 10      24hour > output/speedyone-L2_10_SR_24_hour_mod_ts_and_convection.out &
+nohup time ./run.sh speedyone_L2_50_RN_24_hour_mod_ts_and_diffusion 010 2 SRoff50 24hour > output/speedyone-L2_50_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L2_23_RN_24_hour_mod_ts_and_diffusion 010 2 SRoff23 24hour > output/speedyone-L2_23_RN_24_hour_mod_ts_and_diffusion.out &
+nohup time ./run.sh speedyone_L2_10_RN_24_hour_mod_ts_and_diffusion 010 2 SRoff10 24hour > output/speedyone-L2_10_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L2_10_SR_24_hour_mod_ts_and_diffusion 010 2 10      24hour > output/speedyone-L2_10_SR_24_hour_mod_ts_and_diffusion.out &
 
 
 #L3 - L1 + ablco2 level 6---> 21
-#nohup time ./run.sh speedyone_L3_50_RN_24_hour_mod_ts_and_convection 010 1 SRoff50 24hour_ablco2 > output/speedyone-L3_50_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L3_23_RN_24_hour_mod_ts_and_convection 010 1 SRoff23 24hour_ablco2 > output/speedyone-L3_23_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L3_10_RN_24_hour_mod_ts_and_convection 010 1 SRoff10 24hour_ablco2 > output/speedyone-L3_10_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L3_10_SR_24_hour_mod_ts_and_convection 010 1 10      24hour_ablco2 > output/speedyone-L3_10_SR_24_hour_mod_ts_and_convection.out &
+#nohup time ./run.sh speedyone_L3_50_RN_24_hour_mod_ts_and_diffusion 010 1 SRoff50 24hour_ablco2 > output/speedyone-L3_50_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L3_23_RN_24_hour_mod_ts_and_diffusion 010 1 SRoff23 24hour_ablco2 > output/speedyone-L3_23_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L3_10_RN_24_hour_mod_ts_and_diffusion 010 1 SRoff10 24hour_ablco2 > output/speedyone-L3_10_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L3_10_SR_24_hour_mod_ts_and_diffusion 010 1 10      24hour_ablco2 > output/speedyone-L3_10_SR_24_hour_mod_ts_and_diffusion.out &
 
 
 #L4- Both i.e. 4xCO2 run
-#nohup time ./run.sh speedyone_L4_50_RN_24_hour_mod_ts_and_convection 010 2 SRoff50 24hour_ablco2 > output/speedyone-L4_50_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L4_23_RN_24_hour_mod_ts_and_convection 010 2 SRoff23 24hour_ablco2 > output/speedyone-L4_23_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L4_10_RN_24_hour_mod_ts_and_convection 010 2 SRoff10 24hour_ablco2 > output/speedyone-L4_10_RN_24_hour_mod_ts_and_convection.out &
-#nohup time ./run.sh speedyone_L4_10_SR_24_hour_mod_ts_and_convection 010 2 10      24hour_ablco2 > output/speedyone-L4_10_SR_24_hour_mod_ts_and_convection.out &
+#nohup time ./run.sh speedyone_L4_50_RN_24_hour_mod_ts_and_diffusion 010 2 SRoff50 24hour_ablco2 > output/speedyone-L4_50_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L4_23_RN_24_hour_mod_ts_and_diffusion 010 2 SRoff23 24hour_ablco2 > output/speedyone-L4_23_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L4_10_RN_24_hour_mod_ts_and_diffusion 010 2 SRoff10 24hour_ablco2 > output/speedyone-L4_10_RN_24_hour_mod_ts_and_diffusion.out &
+#nohup time ./run.sh speedyone_L4_10_SR_24_hour_mod_ts_and_diffusion 010 2 10      24hour_ablco2 > output/speedyone-L4_10_SR_24_hour_mod_ts_and_diffusion.out &
 
 
 
