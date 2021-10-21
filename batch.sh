37a387f3ef6aed0c93de57e32bedd3d52542502e
Tom Kimpson <tom.kimpson@mindfoundry.ai>
2021-10-21 16:38:05 +0100

    try just rp1

--- batch.sh
+++ batch.sh
@@ -19,16 +19,22 @@
 
 nohup time ./run.sh speedyoneV2_L2_50_RN_1month 010 2 SRoff50                         1month > output/speedyoneV2-L2_50_RN_1month.out &
 nohup time ./run.sh speedyoneV2_L2_10_RN_1month 010 2 SRoff10                         1month > output/speedyoneV2-L2_10_RN_1month.out &
-nohup time ./run.sh speedyoneV2_L2_10_SR_1month 010 2 10                              1month > output/speedyoneV2-L2_10_SR_1month.out &
+nohup time ./run.sh speedyoneV2_L2_50_RNmod_1month 010 2 SRoff50mod                         1month > output/speedyoneV2-L2_50_RNmod_1month.out &
+nohup time ./run.sh speedyoneV2_L2_10_RNmod_1month 010 2 SRoff10mod                         1month > output/speedyoneV2-L2_10_RNmod_1month.out &
 
-nohup time ./run.sh speedyoneV2_L2_50_RNRP1_1month 010 2 SRoff50_rp_atm2sea1_         1month > output/speedyoneV2-L2_50_RNRP1_1month.out &
-nohup time ./run.sh speedyoneV2_L2_50_RNRP2_1month 010 2 SRoff50_rp_atm2sea2_         1month > output/speedyoneV2-L2_50_RNRP2_1month.out &
-nohup time ./run.sh speedyoneV2_L2_50_RNRP3_1month 010 2 SRoff50_rp_atm2sea3_         1month > output/speedyoneV2-L2_50_RNRP3_1month.out &
-nohup time ./run.sh speedyoneV2_L2_50_RNRP4_1month 010 2 SRoff50_rp_atm2sea4_         1month > output/speedyoneV2-L2_50_RNRP4_1month.out &
-nohup time ./run.sh speedyoneV2_L2_50_RNRP5_1month 010 2 SRoff50_rp_atm2sea5_         1month > output/speedyoneV2-L2_50_RNRP5_1month.out &
-nohup time ./run.sh speedyoneV2_L2_50_RNRP6_1month 010 2 SRoff50_rp_atm2sea6_         1month > output/speedyoneV2-L2_50_RNRP6_1month.out &
-nohup time ./run.sh speedyoneV2_L2_50_RNRP7_1month 010 2 SRoff50_rp_atm2sea7_         1month > output/speedyoneV2-L2_50_RNRP7_1month.out &
-nohup time ./run.sh speedyoneV2_L2_50_RNSEAOUT_1month 010 2 SRoff50_rpsea_out_        1month > output/speedyoneV2-L2_50_RNseaOUT_1month.out &
+
+
+#nohup time ./run.sh speedyoneV2_L2_10_SR_1month 010 2 10                              1month > output/speedyoneV2-L2_10_SR_1month.out &
+# nohup time ./run.sh speedyoneV2_L2_50_RNRP1_1month 010 2 SRoff50_rp_atm2sea1_         1month > output/speedyoneV2-L2_50_RNRP1_1month.out &
+
+
+# nohup time ./run.sh speedyoneV2_L2_50_RNRP2_1month 010 2 SRoff50_rp_atm2sea2_         1month > output/speedyoneV2-L2_50_RNRP2_1month.out &
+# nohup time ./run.sh speedyoneV2_L2_50_RNRP3_1month 010 2 SRoff50_rp_atm2sea3_         1month > output/speedyoneV2-L2_50_RNRP3_1month.out &
+# nohup time ./run.sh speedyoneV2_L2_50_RNRP4_1month 010 2 SRoff50_rp_atm2sea4_         1month > output/speedyoneV2-L2_50_RNRP4_1month.out &
+# nohup time ./run.sh speedyoneV2_L2_50_RNRP5_1month 010 2 SRoff50_rp_atm2sea5_         1month > output/speedyoneV2-L2_50_RNRP5_1month.out &
+# nohup time ./run.sh speedyoneV2_L2_50_RNRP6_1month 010 2 SRoff50_rp_atm2sea6_         1month > output/speedyoneV2-L2_50_RNRP6_1month.out &
+# nohup time ./run.sh speedyoneV2_L2_50_RNRP7_1month 010 2 SRoff50_rp_atm2sea7_         1month > output/speedyoneV2-L2_50_RNRP7_1month.out &
+# nohup time ./run.sh speedyoneV2_L2_50_RNSEAOUT_1month 010 2 SRoff50_rpsea_out_        1month > output/speedyoneV2-L2_50_RNseaOUT_1month.out &
 
 
 
