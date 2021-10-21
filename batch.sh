4587b52ce5cceaec1f768c6df7fc035ed4d5a3cb
Tom Kimpson <tomkimpson@gmail.com>
2021-10-21 17:38:44 +0100

    added SR to the 1 year run

--- batch.sh
+++ batch.sh
@@ -23,6 +23,7 @@
 nohup time ./run.sh speedyoneV2_L2_50_RN_1year 010 2 SRoff50                               1year > output/speedyoneV2-L2_50_RN_1year.out &
 nohup time ./run.sh speedyoneV2_L2_23_RN_1year 010 2 SRoff23                               1year > output/speedyoneV2-L2_23_RN_1year.out &
 nohup time ./run.sh speedyoneV2_L2_10_RN_1year 010 2 SRoff10                               1year > output/speedyoneV2-L2_10_RN_1year.out &
+nohup time ./run.sh speedyoneV2_L2_10_SR_1year 010 2 10                                    1year > output/speedyoneV2-L2_10_SR_1year.out &
 nohup time ./run.sh speedyoneV2_L2_50_RNmod_1year 010 2 SRoff50mod                         1year > output/speedyoneV2-L2_50_RNmod_1year.out &
 nohup time ./run.sh speedyoneV2_L2_10_RNmod_1year 010 2 SRoff10mod                         1year > output/speedyoneV2-L2_10_RNmod_1year.out &
 
