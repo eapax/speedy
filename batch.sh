2f0a33589559624c6e2908770a8a1130aa408a4f
Tom Kimpson <tom.kimpson@mindfoundry.ai>
2021-10-22 12:28:51 +0100

    badger at one year

--- batch.sh
+++ batch.sh
@@ -1,16 +1,16 @@
 
-nohup time ./run.sh speedyonebadger_L2_50_RN_1month 010 2 SRoff50                                1month > output/speedyonebadger-L2_50_RN_1month.out &
-nohup time ./run.sh speedyonebadger_L2_50_RN1_1month 010 2 SRoff50_agcm_1day                     1month > output/speedyonebadger-L2_50_RN1_1month.out &
-nohup time ./run.sh speedyonebadger_L2_50_RN2_1month 010 2 SRoff50_agcm_to_coupler               1month > output/speedyonebadger-L2_50_RN2_1month.out &
-nohup time ./run.sh speedyonebadger_L2_50_RN3_1month 010 2 SRoff50_coupler_to_agcm               1month > output/speedyonebadger-L2_50_RN3_1month.out &
-nohup time ./run.sh speedyonebadger_L2_50_RN4_1month 010 2 SRoff50_fordate                       1month > output/speedyonebadger-L2_50_RN4_1month.out &
-nohup time ./run.sh speedyonebadger_L2_50_RN5_1month 010 2 SRoff50_inifluxes                     1month > output/speedyonebadger-L2_50_RN5_1month.out &
-nohup time ./run.sh speedyonebadger_L2_50_RN6_1month 010 2 SRoff50_stloop                        1month > output/speedyonebadger-L2_50_RN6_1month.out &
-nohup time ./run.sh speedyonebadger_L2_50_RN7_1month 010 2 SRoff50_step                          1month > output/speedyonebadger-L2_50_RN7_1month.out &
-nohup time ./run.sh speedyonebadger_L2_50_RN8_1month 010 2 SRoff50_diagns                        1month > output/speedyonebadger-L2_50_RN8_1month.out &
-nohup time ./run.sh speedyonebadger_L2_23_RN_1month 010 2 SRoff23                                1month > output/speedyonebadger-L2_23_RN_1month.out &
-nohup time ./run.sh speedyonebadger_L2_10_RN_1month 010 2 SRoff10                                1month > output/speedyonebadger-L2_10_RN_1month.out &
-nohup time ./run.sh speedyonebadger_L2_10_SR_1month 010 2 10                                     1month > output/speedyonebadger-L2_10_SR_1month.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN_1year 010 2 SRoff50                                1year > output/speedyonebadger-L2_50_RN_1year.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN1_1year 010 2 SRoff50_agcm_1day                     1year > output/speedyonebadger-L2_50_RN1_1year.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN2_1year 010 2 SRoff50_agcm_to_coupler               1year > output/speedyonebadger-L2_50_RN2_1year.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN3_1year 010 2 SRoff50_coupler_to_agcm               1year > output/speedyonebadger-L2_50_RN3_1year.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN4_1year 010 2 SRoff50_fordate                       1year > output/speedyonebadger-L2_50_RN4_1year.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN5_1year 010 2 SRoff50_inifluxes                     1year > output/speedyonebadger-L2_50_RN5_1year.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN6_1year 010 2 SRoff50_stloop                        1year > output/speedyonebadger-L2_50_RN6_1year.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN7_1year 010 2 SRoff50_step                          1year > output/speedyonebadger-L2_50_RN7_1year.out &
+nohup time ./run.sh speedyonebadger_L2_50_RN8_1year 010 2 SRoff50_diagns                        1year > output/speedyonebadger-L2_50_RN8_1year.out &
+nohup time ./run.sh speedyonebadger_L2_23_RN_1year 010 2 SRoff23                                1year > output/speedyonebadger-L2_23_RN_1year.out &
+nohup time ./run.sh speedyonebadger_L2_10_RN_1year 010 2 SRoff10                                1year > output/speedyonebadger-L2_10_RN_1year.out &
+nohup time ./run.sh speedyonebadger_L2_10_SR_1year 010 2 10                                     1year > output/speedyonebadger-L2_10_SR_1year.out &
 
 
 
