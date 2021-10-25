a6f3bdd977cb3e02ba0a2bb8e40f9802fca05b77
Tom Kimpson <tom.kimpson@mindfoundry.ai>
2021-10-25 18:26:02 +0100

    correct typos

--- batch.sh
+++ batch.sh
@@ -1,6 +1,6 @@
 nohup time ./run.sh speedyoneflamingo_L2_50_RN_10day                 010 2 SRoff50                                10day > output/speedyoneflamingo-L2_50_RN_10day.out &
-nohup time ./run.sh speedyoneflamingo_L2_50_RNagcm_to_coupler_10day  010 2 SRoff50_agcm_to_coupler                10day > output/speedyoneflamingo-L2_50_RNagcm_to_coupler_10day.out &
-nohup time ./run.sh speedyoneflamingo_L2_50_RNcoupler_to_agcm _10day 010 2 SRoff50_coupler_to_agcm                10day > output/speedyoneflamingo-L2_50_RNcoupler_to_agcm_10day.out &
+nohup time ./run.sh speedyoneflamingo_L2_50_RNagcmtocoupler_10day  010 2 SRoff50_agcm_to_coupler                10day > output/speedyoneflamingo-L2_50_RNagcm_to_coupler_10day.out &
+nohup time ./run.sh speedyoneflamingo_L2_50_RNcouplertoagcm_10day 010 2 SRoff50_coupler_to_agcm                10day > output/speedyoneflamingo-L2_50_RNcoupler_to_agcm_10day.out &
 nohup time ./run.sh speedyoneflamingo_L2_50_RNcoupler_10day          010 2 SRoff50_coupler                        10day > output/speedyoneflamingo-L2_50_RNcoupler_10day.out &
 
 
