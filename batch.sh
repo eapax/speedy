dc8342c7967f9251df777a7129395b8bc8f463d9
Tom Kimpson <tom.kimpson@mindfoundry.ai>
2021-10-26 16:23:44 +0100

    JACKAL: 1year

--- batch.sh
+++ batch.sh
@@ -1,7 +1,7 @@
-nohup time ./run.sh speedyonejackal_L2_50_RN_10day                 010 2 SRoff50                                10day > output/speedyonejackal-L2_50_RN_10day.out &
-nohup time ./run.sh speedyonejackal_L2_23_RN_10day                 010 2 SRoff23                                10day > output/speedyonejackal-L2_23_RN_10day.out &
-nohup time ./run.sh speedyonejackal_L2_10_RN_10day                 010 2 SRoff10                                10day > output/speedyonejackal-L2_10_RN_10day.out &
-#nohup time ./run.sh speedyonejackal_L2_10_SR_10day                 010 2 10                                     10day > output/speedyonejackal-L2_10_SR_10day.out &
+nohup time ./run.sh speedyonejackal_L2_50_RN_1year                 010 2 SRoff50                                1year > output/speedyonejackal-L2_50_RN_1year.out &
+nohup time ./run.sh speedyonejackal_L2_23_RN_1year                 010 2 SRoff23                                1year > output/speedyonejackal-L2_23_RN_1year.out &
+nohup time ./run.sh speedyonejackal_L2_10_RN_1year                 010 2 SRoff10                                1year > output/speedyonejackal-L2_10_RN_1year.out &
+nohup time ./run.sh speedyonejackal_L2_10_SR_1year                 010 2 10                                     1year > output/speedyonejackal-L2_10_SR_1year.out &
 
 
  
