8093f5d9619b4338bdd0cef16a3b7ed83bf21ed0
Tom Kimpson <tom.kimpson@mindfoundry.ai>
2021-10-27 17:23:41 +0100

    and run for 1 year

--- batch.sh
+++ batch.sh
@@ -1,16 +1,16 @@
 
-nohup time ./run.sh speedyonekangaroo_L2_50_RN_1month                 010 2 SRoff50                                   1month > output/speedyonekangaroo-L2_50_RN_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_10_RN_1month                 010 2 SRoff10                                   1month > output/speedyonekangaroo-L2_10_RN_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_10_RNagcm_1month             010 2 SRoff10_agcm                              1month > output/speedyonekangaroo-L2_10_RNagcm_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_10_RNcpl_1month              010 2 SRoff10_coupler                           1month > output/speedyonekangaroo-L2_10_RNcoupler_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_10_RNhalf_1month             010 2 SRoff10_half                              1month > output/speedyonekangaroo-L2_10_RNhalf_1month.out &
+nohup time ./run.sh speedyonekangaroo_L2_50_RN_1year                 010 2 SRoff50                                   1year > output/speedyonekangaroo-L2_50_RN_1year.out &
+nohup time ./run.sh speedyonekangaroo_L2_10_RN_1year                 010 2 SRoff10                                   1year > output/speedyonekangaroo-L2_10_RN_1year.out &
+nohup time ./run.sh speedyonekangaroo_L2_10_RNagcm_1year             010 2 SRoff10_agcm                              1year > output/speedyonekangaroo-L2_10_RNagcm_1year.out &
+nohup time ./run.sh speedyonekangaroo_L2_10_RNcpl_1year              010 2 SRoff10_coupler                           1year > output/speedyonekangaroo-L2_10_RNcoupler_1year.out &
+nohup time ./run.sh speedyonekangaroo_L2_10_RNhalf_1year             010 2 SRoff10_half                              1year > output/speedyonekangaroo-L2_10_RNhalf_1year.out &
 
 
 
-#nohup time ./run.sh speedyonekangaroo_L2_50_RN_1month                 010 2 SRoff50                                1month > output/speedyonekangaroo-L2_50_RN_1month.out &
-#nohup time ./run.sh speedyonekangaroo_L2_50_RNagcm_1month             010 2 SRoff50_agcm                                1month > output/speedyonekangaroo-L2_50_RNagcm_1month.out &
-#nohup time ./run.sh speedyonekangaroo_L2_50_RNcoupler_1month          010 2 SRoff50_coupler                                1month > output/speedyonekangaroo-L2_50_RNcoupler_1month.out &
-#nohup time ./run.sh speedyonekangaroo_L2_10_RN_1month                 010 2 SRoff10                                1month > output/speedyonekangaroo-L2_10_RN_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RN_1year                 010 2 SRoff50                                1year > output/speedyonekangaroo-L2_50_RN_1year.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RNagcm_1year             010 2 SRoff50_agcm                                1year > output/speedyonekangaroo-L2_50_RNagcm_1year.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RNcoupler_1year          010 2 SRoff50_coupler                                1year > output/speedyonekangaroo-L2_50_RNcoupler_1year.out &
+#nohup time ./run.sh speedyonekangaroo_L2_10_RN_1year                 010 2 SRoff10                                1year > output/speedyonekangaroo-L2_10_RN_1year.out &
 
 
 
