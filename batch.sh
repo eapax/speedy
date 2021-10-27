7a6c9f212845ae0c1b3c9ad0b76115de4d2de13f
Tom Kimpson <tom.kimpson@mindfoundry.ai>
2021-10-27 12:25:24 +0100

    add a combination precision

--- batch.sh
+++ batch.sh
@@ -1,8 +1,10 @@
-nohup time ./run.sh speedyonekangaroo_L2_50_RN_1month                 010 2 SRoff50                                1month > output/speedyonekangaroo-L2_50_RN_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_50_RNfordate_1month          010 2 SRoff50_fordate                        1month > output/speedyonekangaroo-L2_50_RNfordate_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_50_RNinifluxes_1month        010 2 SRoff50_inifluxes                      1month > output/speedyonekangaroo-L2_50_RNinifluxes_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_50_RNstloop_1month           010 2 SRoff50_stloop                         1month > output/speedyonekangaroo-L2_50_RNstloop_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_10_RN_1month                 010 2 SRoff10                                1month > output/speedyonekangaroo-L2_10_RN_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RN_1month                 010 2 SRoff50                                1month > output/speedyonekangaroo-L2_50_RN_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RNfordate_1month          010 2 SRoff50_fordate                        1month > output/speedyonekangaroo-L2_50_RNfordate_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RNinifluxes_1month        010 2 SRoff50_inifluxes                      1month > output/speedyonekangaroo-L2_50_RNinifluxes_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RNstloop_1month           010 2 SRoff50_stloop                         1month > output/speedyonekangaroo-L2_50_RNstloop_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_10_RN_1month                 010 2 SRoff10                                1month > output/speedyonekangaroo-L2_10_RN_1month.out &
+
+nohup time ./run.sh speedyonekangaroo_L2_50_RNcombo_1month           010 2 SRoff50_combo                         1month > output/speedyonekangaroo-L2_50_RNcombo_1month.out &
 
 
  
