d2651414e7687c9171cf72f253e0c366aa5d3a7a
Tom Kimpson <tom.kimpson@mindfoundry.ai>
2021-10-27 13:56:55 +0100

    also consider init

--- batch.sh
+++ batch.sh
@@ -1,7 +1,11 @@
-nohup time ./run.sh speedyonekangaroo_L2_50_RN_1month                 010 2 SRoff50                                1month > output/speedyonekangaroo-L2_50_RN_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_50_RNagcm_1month             010 2 SRoff50_agcm                                1month > output/speedyonekangaroo-L2_50_RNagcm_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_50_RNcoupler_1month          010 2 SRoff50_coupler                                1month > output/speedyonekangaroo-L2_50_RNcoupler_1month.out &
-nohup time ./run.sh speedyonekangaroo_L2_10_RN_1month                 010 2 SRoff10                                1month > output/speedyonekangaroo-L2_10_RN_1month.out &
+
+nohup time ./run.sh speedyonekangaroo_L2_50_RNinit_1month                 010 2 SRoff50_init                                1month > output/speedyonekangaroo-L2_50_RNinit_1month.out &
+
+
+#nohup time ./run.sh speedyonekangaroo_L2_50_RN_1month                 010 2 SRoff50                                1month > output/speedyonekangaroo-L2_50_RN_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RNagcm_1month             010 2 SRoff50_agcm                                1month > output/speedyonekangaroo-L2_50_RNagcm_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_50_RNcoupler_1month          010 2 SRoff50_coupler                                1month > output/speedyonekangaroo-L2_50_RNcoupler_1month.out &
+#nohup time ./run.sh speedyonekangaroo_L2_10_RN_1month                 010 2 SRoff10                                1month > output/speedyonekangaroo-L2_10_RN_1month.out &
 
 
 
