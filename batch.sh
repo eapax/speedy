a58d1087efb4a4fd45d87e1d6ab0c869c85ff676
Tom Kimpson <tom.kimpson@mindfoundry.ai>
2021-10-30 23:32:59 +0100

    check agcm and init with low prec coupling

--- batch.sh
+++ batch.sh
@@ -5,10 +5,8 @@
 #nohup time ./run.sh speedyonellama_L2_10_RNstloop_1year           010 2 SRoff10_stloop                       1year > output/speedyonellama-L2_10_RNstloop_1year.out &
 
 
-nohup time ./run.sh speedyoneflamingo_L2_50_RN_1year                     010 2 SRoff50                             1year > output/speedyoneflamingo-L2_50_RN.out &
-nohup time ./run.sh speedyoneflamingo_L2_50_RNcoupler_1year              010 2 SRoff50_coupler                     1year > output/speedyoneflamingo-L2_50_RNcpl.out &
-nohup time ./run.sh speedyoneflamingo_L2_50_RNcouplercorrected_1year     010 2 SRoff50_coupler_corrected           1year > output/speedyoneflamingo-L2_50_RNcplcorr.out &
-nohup time ./run.sh speedyoneflamingo_L2_10_RN_1year                     010 2 SRoff10                             1year > output/speedyoneflamingo-L2_10_RN.out &
+nohup time ./run.sh speedyoneflamingo_L2_50_RNagcm_1year              010 2 SRoff50_agcm          1year > output/speedyoneflamingo-L2_50_RNagcm.out &
+nohup time ./run.sh speedyoneflamingo_L2_50_RNinit_1year              010 2 SRoff50_init          1year > output/speedyoneflamingo-L2_50_RNinit.out &
 
 
 
