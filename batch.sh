190b51ae2c148ea86b3c4e7840b1d386cc8b42a5
Tom Kimpson <tomkimpson@gmail.com>
2021-11-07 08:57:13 +0000

    alpha for 10years

--- batch.sh
+++ batch.sh
@@ -6,7 +6,7 @@
 
 
  
-nohup time ./run.sh speedyoneALPHA_L2_10_RN_1year           010 2 SRoff10       1year > output/speedyoneALPHA-L2_10_RN.out &
-nohup time ./run.sh speedyoneALPHA_L2_10_SR_1year           010 2 10            1year > output/speedyoneALPHA-L2_10_SR.out &
-nohup time ./run.sh speedyoneALPHA_L2_10_RNinit_1year       010 2 SRoff10_init  1year > output/speedyoneALPHA-L2_10_RNinit.out &
-nohup time ./run.sh speedyoneALPHA_L2_10_SRinit_1year       010 2 10_init       1year > output/speedyoneALPHA-L2_10_SRinit.out &
+nohup time ./run.sh speedyoneALPHA_L2_10_RN_10year           010 2 SRoff10       10year > output/speedyoneALPHA-L2_10_RN_10yr.out &
+#nohup time ./run.sh speedyoneALPHA_L2_10_SR_1year           010 2 10            1year > output/speedyoneALPHA-L2_10_SR.out &
+#nohup time ./run.sh speedyoneALPHA_L2_10_RNinit_1year       010 2 SRoff10_init  1year > output/speedyoneALPHA-L2_10_RNinit.out &
+#nohup time ./run.sh speedyoneALPHA_L2_10_SRinit_1year       010 2 10_init       1year > output/speedyoneALPHA-L2_10_SRinit.out &
