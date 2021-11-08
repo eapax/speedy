4570ef1a6f8935735b15adf75794acd9a1dfac0b
Tom Kimpson <tomkimpson@gmail.com>
2021-11-08 08:45:23 +0000

    10year run

--- batch.sh
+++ batch.sh
@@ -1,18 +1,19 @@
-# nohup time ./run.sh speedyoneglenfiddich_L2_50_RN50gridfields_1year         010 2 SRoff50_gridfields      1year > output/speedyoneglenfiddich-L2_50_RNgridfields.out &
-# nohup time ./run.sh speedyoneglenfiddich_L2_50_RN50dyntend_1year         010 2 SRoff50_dyntend      1year > output/speedyoneglenfiddich-L2_50_RNdyntend.out &
-# nohup time ./run.sh speedyoneglenfiddich_L2_50_RN50phypar_1year         010 2 SRoff50_phypar      1year > output/speedyoneglenfiddich-L2_50_RNphypar.out &
-# nohup time ./run.sh speedyoneglenfiddich_L2_50_RN10mod_1year         010 2 SRoff10_modgeop      1year > output/speedyoneglenfiddich-L2_10_RNmod.out &
+# nohup time ./run.sh speedyoneglenfiddich_L2_50_RN50gridfields_10year         010 2 SRoff50_gridfields      10year > output/speedyoneglenfiddich-L2_50_RNgridfields.out &
+# nohup time ./run.sh speedyoneglenfiddich_L2_50_RN50dyntend_10year         010 2 SRoff50_dyntend      10year > output/speedyoneglenfiddich-L2_50_RNdyntend.out &
+# nohup time ./run.sh speedyoneglenfiddich_L2_50_RN50phypar_10year         010 2 SRoff50_phypar      10year > output/speedyoneglenfiddich-L2_50_RNphypar.out &
+# nohup time ./run.sh speedyoneglenfiddich_L2_50_RN10mod_10year         010 2 SRoff10_modgeop      10year > output/speedyoneglenfiddich-L2_10_RNmod.out &
 
 
 
  
-nohup time ./run.sh speedyoneBETA_L2_10_RNfordate_1year           010 2 SRoff10_fordate       1year > output/speedyoneBETA-L2_10_RNfordate_1yr.out &
-nohup time ./run.sh speedyoneBETA_L2_10_RNforin5_1year            010 2 SRoff10_forin5        1year > output/speedyoneBETA-L2_10_RNforin5_1yr.out &
-nohup time ./run.sh speedyoneBETA_L2_10_RNtimeint_1year           010 2 SRoff10_timeint       1year > output/speedyoneBETA-L2_10_RNtimeint_1yr.out &
+nohup time ./run.sh speedyoneBETA_L2_10_RNfordate_10year           010 2 SRoff10_fordate       10year > output/speedyoneBETA-L2_10_RNfordate_10yr.out &
+nohup time ./run.sh speedyoneBETA_L2_10_RNforin5_10year            010 2 SRoff10_forin5        10year > output/speedyoneBETA-L2_10_RNforin5_10yr.out &
+nohup time ./run.sh speedyoneBETA_L2_10_RNtimeint_10year           010 2 SRoff10_timeint       10year > output/speedyoneBETA-L2_10_RNtimeint_10yr.out &
+nohup time ./run.sh speedyoneBETA_L2_10_SR_10year                  010 2 10                    10year > output/speedyoneBETA-L2_10_SR_10yr.out &
 
 
 
 
-#nohup time ./run.sh speedyoneBETA_L2_10_SR_1year           010 2 10            1year > output/speedyoneBETA-L2_10_SR.out &
-#nohup time ./run.sh speedyoneBETA_L2_10_RNinit_1year       010 2 SRoff10_init  1year > output/speedyoneBETA-L2_10_RNinit.out &
-#nohup time ./run.sh speedyoneBETA_L2_10_SRinit_1year       010 2 10_init       1year > output/speedyoneBETA-L2_10_SRinit.out &
+#nohup time ./run.sh speedyoneBETA_L2_10_SR_10year           010 2 10            10year > output/speedyoneBETA-L2_10_SR.out &
+#nohup time ./run.sh speedyoneBETA_L2_10_RNinit_10year       010 2 SRoff10_init  10year > output/speedyoneBETA-L2_10_RNinit.out &
+#nohup time ./run.sh speedyoneBETA_L2_10_SRinit_10year       010 2 10_init       10year > output/speedyoneBETA-L2_10_SRinit.out &
