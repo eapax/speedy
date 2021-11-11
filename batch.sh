f7ee1c53cf158233edf14315c80e36b4676c6a81
Tom Kimpson <tomkimpson@gmail.com>
2021-11-11 09:48:15 +0000

    Fig1

--- batch.sh
+++ batch.sh
@@ -6,9 +6,12 @@
 
 
  
-nohup time ./run.sh speedyonetimeintLF_L2_50_RN           010 2 SRoff50           1yearLF > output/speedyonetimeintLF-L2_50_RN.out &
-nohup time ./run.sh speedyonetimeintLF_L2_50_RNtimeint    010 2 SRoff50_timeint   1yearLF > output/speedyonetimeintLF-L2_50_RNtimeint.out &
-nohup time ./run.sh speedyonetimeintLF_L2_50_SRtimeint    010 2 50timeint         1yearLF > output/speedyonetimeintLF-L2_50_SRtimeint.out &
+nohup time ./run.sh speedyoneFIG1_L2_52_RN           010 2 SRoff52           1year4CO2 > output/speedyoneFIG1-L2_52_RN.out &
+nohup time ./run.sh speedyoneFIG1_L2_23_RN           010 2 SRoff23           1year4CO2 > output/speedyoneFIG1-L2_23_RN.out &
+nohup time ./run.sh speedyoneFIG1_L2_10_RN           010 2 SRoff10           1year4CO2 > output/speedyoneFIG1-L2_10_RN.out &
+nohup time ./run.sh speedyoneFIG1_L2_52_SR           010 2 52                1year4CO2 > output/speedyoneFIG1-L2_52_SR.out &
+nohup time ./run.sh speedyoneFIG1_L2_23_SR           010 2 23                1year4CO2 > output/speedyoneFIG1-L2_23_SR.out &
+nohup time ./run.sh speedyoneFIG1_L2_10_SR           010 2 10                1year4CO2 > output/speedyoneFIG1-L2_10_SR.out &
 
 
 #nohup time ./run.sh speedyoneforin5_L2_50_SR           010 2 50              10day_iseasc > output/speedyoneforin5-L2_50_SR.out &
