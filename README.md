7341e29f253c18292276cc0e0da607febf26a989
Tom Kimpson <tomkimpson@gmail.com>
2021-11-17 13:44:54 +0000

    also do breakdowns by pain point for 10 years

--- README.md
+++ README.md
@@ -5,9 +5,20 @@ Please see the original repo and references therein.
 
 Changes in this branch compared to the original can be see at the [git compare](https://github.com/eapax/speedy/compare/stochastic_rounding...tomkimpson:stochastic_rounding)
 
-There is also a draft PR here https://github.com/eapax/speedy/pull/1/files?authenticity_token=cqTBaTfw4aA%2BiSy2MM%2BM4N%2Fi3irhhFayvmsIvasOrR1DSfce6yh6VTdmt%2Bf%2BO8lpkNicSOt1qxsCAIurT2%2BziQ%3D%3D&file-filters%5B%5D=.f90&hide-deleted-files=true
+There is also a draft PR here https://github.com/eapax/speedy/pull/1/files
 
 note we have filtered to just show changes to source code (`.f90`) - does not show any changes to e.g. namelist files.
+
+
+Major changes
+
+geop
+
+removed most set precision options - now set once globally, with the exception of the following:
+
+
+
+Minor changes
 
 * Remove nc from gitignore so we have copies of the input files in the remote
 
--- batch.sh
+++ batch.sh
@@ -9,11 +9,12 @@
 #nohup time ./run.sh speedyoneFIG1_L2_10_SR_10year            010 2 10                10year4CO2 > output/speedyoneFIG1-L2_10_SR_10year.out &
 
 
-nohup time ./run.sh speedyoneFIG1_L2_10_SR_10year                   010 2 10                10year4CO2 > output/speedyoneFIG1-L2_10_SR_10year.out &
+#nohup time ./run.sh speedyoneFIG1_L2_10_SR_10year                   010 2 10                10year4CO2 > output/speedyoneFIG1-L2_10_SR_10year.out &
 
-#nohup time ./run.sh speedyoneBREAKDOWN_L2_50_RNcoupling_10year      010 2 SRoff52_cpl       10year4CO2 > output/speedyoneBREAKDOWN-L2_50_RNcpl_10year.out &
-#nohup time ./run.sh speedyoneBREAKDOWN_L2_50_RNtimestep_10year      010 2 SRoff52_timeint   10year4CO2 > output/speedyoneBREAKDOWN-L2_50_RNtimeint_10year.out &
-#nohup time ./run.sh speedyoneBREAKDOWN_L2_50_RNcompound_10year      010 2 SRoff52_compound  10year4CO2 > output/speedyoneBREAKDOWN-L2_50_RNcompound_10year.out &
+nohup time ./run.sh speedyoneBREAKDOWN_L2_50_RNcoupling_10year      010 2 SRoff52_cpl       10year4CO2 > output/speedyoneBREAKDOWN-L2_50_RNcpl_10year.out &
+nohup time ./run.sh speedyoneBREAKDOWN_L2_50_RNtimestep_10year      010 2 SRoff52_timeint   10year4CO2 > output/speedyoneBREAKDOWN-L2_50_RNtimeint_10year.out &
+nohup time ./run.sh speedyoneBREAKDOWN_L2_50_RNcompound_10year      010 2 SRoff52_compound  10year4CO2 > output/speedyoneBREAKDOWN-L2_50_RNcompound_10year.out &
+
 #nohup time ./run.sh speedyoneHALF_L2_10H_SR_10year                  010 2 10H               10year4CO2 > output/speedyoneHALF_L2_10H_SR_10year.out &
 #nohup time ./run.sh speedyoneHALF_L2_10H_RN_10year                  010 2 SRoff10H          10year4CO2 > output/speedyoneHALF_L2_10H_RN_10year.out &
 
