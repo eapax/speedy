cb6b1915dbf697da87d4990ce41b4f4da32d7a34
Tom Kimpson <tomkimpson@gmail.com>
2021-12-14 11:26:55 +0000

    last one

--- source/dyn_step.f90
+++ source/dyn_step.f90
@@ -184,7 +184,7 @@ subroutine timint(j1,dt,eps,wil,nlev,field,fdt)
     type(rpe_var), intent(in) :: dt, eps, wil
     type(rpe_complex_var), intent(in) :: fdt(mx,nx,nlev)
     type(rpe_complex_var), intent(inout) :: field(mx,nx,nlev,2)
-    type(rpe_complex_var) :: f1(mx,nx,nlev,1)
+    type(rpe_complex_var) :: fieldOG(mx,nx,nlev,2)
     type(rpe_var) :: eps2
     type(rpe_complex_var) :: fnew(mx,nx)
     integer :: k, n, m
@@ -218,14 +218,14 @@ subroutine timint(j1,dt,eps,wil,nlev,field,fdt)
     ! end do
 
 
-
+    fieldOG = field !make a copy of initial state
 
     ! Correct the williams filter 
     do k=1,nlev
         do n=1,nx
             do m=1,mx
                 
-                f1 = field(m,n,k,1) 
+                
 
                 fnew (m,n)     = field(m,n,k,1) + dt*fdt(m,n,k)
                 field(m,n,k,1) = field(m,n,k,j1) + wil*eps*(field(m,n,k,1)&
@@ -233,7 +233,7 @@ subroutine timint(j1,dt,eps,wil,nlev,field,fdt)
 
                 
                 ! and here comes Williams' innovation to the filter
-                field(m,n,k,2) = fnew(m,n) -(rpe_literal(1.0_dp)-wil)*eps*(field(m,n,k,1)&
+                field(m,n,k,2) = fnew(m,n) -(rpe_literal(1.0_dp)-wil)*eps*(fieldOG(m,n,k,1)&
                    &-rpe_literal(2.0_dp)*field(m,n,k,j1)+fnew(m,n)) 
                 
             end do
