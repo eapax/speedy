e26c623257e8a29e72da09db504606bee26cba1a
Tom Kimpson <tomkimpson@gmail.com>
2022-02-08 13:47:11 +0000

    add jupyter notebooks

---
+++ jupyter_notebooks/scripts.py
@@ -0,0 +1,170 @@
+#Import everything
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import glob
+import pandas as pd
+import seaborn as sns
+import sys
+from collections import defaultdict
+from tqdm.notebook import tqdm
+import matplotlib as mpl
+
+
+
+
+
+
+def extract_file_info(d):
+    
+    #Mapping between levels
+    level_dict = {'L1': 'El Nino Static SST. ablco2=6',
+                  'L2': 'EC Earth SST. ablco2=6',
+                  'L3': 'El Nino Static SST. ablco2=21',
+                  'L4': 'EC Earth SST. ablco2=21'
+                  }
+
+    #Create some useful labels
+    label = d.split('/')[-1]
+    level = label.split('_')[1]
+    prec = label.split('_')[2]
+    rounding = label.split('_')[3]
+    title = level_dict[level]
+    
+    keys = ['label', 'level', 'precision','title']
+    values = [label,level,prec+'_'+rounding,title]
+    dictionary = {keys[i]: values[i] for i in range(len(keys))}
+    
+    return dictionary
+
+
+
+
+def surface_slice(y,weights):
+    
+ 
+    #Get values at surface
+    y_surface =  y[:,0]   
+    ygrid = y_surface.weighted(weights).mean(("longitude", "latitude")).data #average across every surface grid point
+
+
+    return ygrid
+
+
+def extract_file_data(df,columns,weights):
+    
+    
+    data = [surface_slice(getattr(df,
+                                  columns[k]),
+                                   weights = weights) for k in range(len(columns))]
+    
+    
+    dictionary = {columns[i]: data[i] for i in range(len(columns))}
+    df_out = pd.DataFrame(dictionary)
+    
+    
+    return df_out
+
+    
+    
+def process_nc_file(fname,weights,directory,true_lat):    
+    
+        #Get data 
+        df = xr.open_dataset(fname)
+        
+        
+        #Reset the latitude
+        df = df.assign_coords(latitude=(true_lat))
+        
+
+       
+        #Get the data you want     
+        columns_3d  = ['temperature']
+        data3d = extract_file_data(df,columns_3d,weights)
+
+                
+        #Get meta information
+        f_info = extract_file_info(directory)
+        index = data3d.index
+        df_info = pd.DataFrame(f_info,index=index)
+      
+        #Create pandas df and append
+        df_meta = pd.concat([data3d,df_info,],axis=1) 
+        
+        return df_meta
+        
+    
+        
+def process_all_data(all_directories,weights,true_lat):
+
+    
+    #Empty arrays to hold data
+    dfs = []
+    
+
+    for d in tqdm(all_directories): #for every directory
+        df_LI = [] # df of solution level i
+        nc_files = sorted(glob.glob(d+'/model_output*.nc'))
+                
+        for n in tqdm(nc_files): #for every model.nc file
+
+            df_meta = process_nc_file(n,weights,d,true_lat)
+            df_LI.append(df_meta)
+        
+        df_LI = pd.concat(df_LI,ignore_index=True)
+        df_LI['X'] = np.arange(len(df_LI))
+        
+        #Add append to bigger array
+        dfs.append(df_LI)
+            
+    #Bring all together
+    df = pd.concat(dfs)
+    
+    return df #.reset_index()
+
+    
+def get_global_weights():
+    
+     #Get the latitude weights from a special location
+    r1 = '/network/group/aopp/predict/TIP016_PAXTON_RPSPEEDY/speedyone/paper/Fig1_10year/'
+    f = r1 + 'speedyoneFIG1_L2_52_RN_10year/model_output00001.nc'
+    df = xr.open_dataset(f)
+    
+    temp_file = df.temperature
+    weights = np.cos(np.deg2rad(temp_file.latitude))
+         
+    weights.name = "weights"
+    
+    
+    return weights, temp_file.latitude
+    
+
+def process_node(node):
+    
+    root= '/network/group/aopp/predict/TIP016_PAXTON_RPSPEEDY/speedyone/'+node
+    
+   
+    #Get the global weights
+    weights,true_lat = get_global_weights()
+    
+    
+    
+    #iterate over every directory
+    all_dirs = glob.glob(root+'speedyone*')     
+    df = process_all_data(all_dirs,weights,true_lat)
+    
+     
+    
+    return df
+
+
+
+
+    
+
+
+
+
+
+
