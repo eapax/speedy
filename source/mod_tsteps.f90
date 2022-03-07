33ad4cb3d449ba787be7db94d8e946cc30ce7257
Tom Kimpson <tomkimpson@gmail.com>
2022-03-07 17:28:22 +0000

    nsteps = 18

--- source/mod_tsteps.f90
+++ source/mod_tsteps.f90
@@ -9,7 +9,7 @@ module mod_tsteps
     namelist /timestepping/ nsteps, nstdia, nmonrs, rob, wil
 
     ! No. of time steps in one day
-    integer :: nsteps = 2
+    integer :: nsteps = 18
 
     ! Period (no. of steps) for diagnostic print-out
     integer :: nstdia = 30
